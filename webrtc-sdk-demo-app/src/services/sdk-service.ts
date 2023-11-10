a4032ecd53c5d7c0350c865dea244f952f3ad40a
Zachary <zach.servies@genesys.com>
2023-11-09 23:43:06 -0500

    Emit pending sessions.

--- webrtc-sdk-demo-app/src/services/sdk-service.ts
+++ webrtc-sdk-demo-app/src/services/sdk-service.ts
@@ -1,6 +1,7 @@
-import  { ISdkConfig, GenesysCloudWebrtcSdk, SdkError } from 'genesys-cloud-webrtc-sdk';
+import  { ISdkConfig, GenesysCloudWebrtcSdk, SdkError, ISessionIdAndConversationId } from 'genesys-cloud-webrtc-sdk';
 import { v4 } from 'uuid';
 import { eventService } from './event-service';
+import { GenesysCloudMediaSession } from 'genesys-cloud-streaming-client';
 
 let webrtcSdk: GenesysCloudWebrtcSdk;
 let pendingSessions: any = [];
@@ -49,25 +50,48 @@ export function startSoftphoneSession (phoneNumber: string) {
     console.error('Must enter a valid phone number.');
     return;
   }
-  console.warn('we got the number', phoneNumber);
   webrtcSdk.startSoftphoneSession({ phoneNumber });
 }
 
-function handlePendingSession(session: any) {
-  console.warn('we have a pending session!')
-  // Check if we already have this pending session -> if not add it to array.
-  const existingPendingSession = pendingSessions.find((s: any) => s.conversationId === session.conversationId);
+function handlePendingSession(pendingSession: ISessionIdAndConversationId) {
+  console.warn('we have a pending session!', pendingSession)
+  // Check if we already have this pending session -> if not add it to array and emit to UI
+  const existingPendingSession = pendingSessions.find((s: any) => s.conversationId === pendingSession.conversationId);
   if (!existingPendingSession) {
-    pendingSessions.push(session);
+    pendingSessions.push(pendingSession);
+    eventService.dispatchEvent('pendingSession', pendingSessions);
   }
 }
 
-function pendingSessionHandled() {
-  console.warn('aready handled boss');
+function pendingSessionHandled(pendingSession: ISessionIdAndConversationId) {
+  console.warn('already handled boss', pendingSession);
+  // Remove pending session from global pendingSessions array and emit new array to UI
+  pendingSessions = pendingSessions.filter((s: any) => s.conversationId !== pendingSession.conversationId);
+  eventService.dispatchEvent('pendingSession', pendingSessions);
 }
 
-function handleSessionStarted() {
-  console.warn('handle session started!')
+function handleSessionStarted(session: GenesysCloudMediaSession) {
+  console.warn('handle session started!', session)
+// conversationUpdatesToRender[session.conversationId] = session;
+
+session.on('connectionState', () => console.warn('state'));
+
+// create metadatas
+// const track = screenStream.getTracks()[0];
+// const { height, width, deviceId } = track.getSettings();
+// const screenRecordingMetadatas = [
+//   {
+//     trackId: track.id,
+//     screenId: deviceId, // some applications give you a deviceId on the track which is uniquely tied to a specific monitor
+//     originX: 0,
+//     originY: 0,
+//     resolutionX: width,
+//     resolutionY: height,
+//   primary: true,
+//   }
+// ];
+
+webrtcSdk.acceptSession({ conversationId: session.conversationId, sessionType: session.sessionType });
 }
 
 function handleSessionEnded() {
